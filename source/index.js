86d2003c71155bb4fd92f61ad0395c094c43772f
Rodrigo Simões <rodjbs@gmail.com>
2017-02-17 17:40:35 +0000

    Fix index.js

--- source/index.js
+++ source/index.js
@@ -1,5 +1,3 @@
-// this script is run when require() is called and should return the module
-// TODO
 'use strict';
-var rpio = require("../../build/Release/rpio.node")
-module.exports = rpio; // <=> {cleanup: rpio.cleanup, ...};?
+var rpio = require("../build/Release/rpio.node")
+module.exports = rpio;
